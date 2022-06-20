6cb688222ef7a76817836a02808f55d1f6288d36
Jay Kapadiya <jayk.mobio@gmail.com>
2022-06-20 14:53:21 +0530

    Different types of datatypes

---
+++ datatypes.py
@@ -0,0 +1,26 @@
+a = 5  
+print("The type of a", type(a))  
+  
+b = 40.5  
+print("The type of b", type(b))  
+  
+c = 1+3j  
+print("The type of c", type(c))
+
+print(" c is a complex number", isinstance(1+3j,complex))  
+
+str = "string using double quotes"  
+print(type(str)) 
+
+list1  = [1, "hi", "Python", 2]    
+print(type(list1))  
+
+tup  = ("hi", "Python", 2)    
+print (type(tup)) 
+
+d = {1:'Jimmy', 2:'Alex', 3:'john', 4:'mike'}     
+print (type(d)) 
+
+set = {'James', 2, 3,'Python'} 
+print (type(set)) 
+
