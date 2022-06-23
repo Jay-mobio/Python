6c91c204cdebe6445d49d739c7ddab591e6c64fe
Jay Kapadiya <jayk.mobio@gmail.com>
2022-06-23 11:42:07 +0530

    Multithreading Programs

---
+++ Multithreding/multithreding1.py
@@ -0,0 +1,17 @@
+import threading
+def cube(num):
+    print("Cube {}".format(num*num*num))
+
+def square(num):
+    print("Square {}".format(num*num))
+
+if __name__ == "__main__":
+
+    t1 = threading.Thread(target=cube, args = (10,))
+    t2 = threading.Thread(target=square, args = (10,))
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
